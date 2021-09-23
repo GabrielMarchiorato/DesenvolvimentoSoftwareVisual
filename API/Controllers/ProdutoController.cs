7eedd4a8df727daef22f939a568803495db29769
GabrielMarchiorato <gabrielmarchiorato1@outlook.com>
2021-09-23 11:35:06 -0300

    Adição do controller

---
+++ API/Controllers/ProdutoController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Models;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/produto")]
+
+    public class ProdutoController : ControllerBase
+    {
+        //POST: api/produto/create
+        [HttpPost]
+        [Route("create")]
+
+        public Produto Create(Produto produto)
+        {
+            produto.Nome += " alterado";
+            return produto;
+        }
+    }
+}

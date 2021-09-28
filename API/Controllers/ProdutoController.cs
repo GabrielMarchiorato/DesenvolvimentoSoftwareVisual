15c2077351b614d485af950dce7179a942c34e6c
GabrielMarchiorato <gabrielmarchiorato1@outlook.com>
2021-09-28 18:02:48 -0300

    Configuração do banco de dados em memória, cadastro e listagem de produtos

--- API/Controllers/ProdutoController.cs
+++ API/Controllers/ProdutoController.cs
@@ -1,5 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
 using API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -8,14 +12,31 @@ namespace API.Controllers
 
     public class ProdutoController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ProdutoController(DataContext context)
+        {
+            _context = context;
+        }
+
         //POST: api/produto/create
         [HttpPost]
         [Route("create")]
 
         public Produto Create(Produto produto)
         {
-            produto.Nome += " alterado";
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
             return produto;
         }
+        //GET: api/produto/list
+        [HttpGet]
+        [Route("list")]
+
+        public List<Produto> List() => _context.Produtos.ToList();
+
+
+
+
     }
 }
